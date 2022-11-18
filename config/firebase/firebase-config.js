7cc17382f68aeb436f63b1a3d7cebbb22b995938
Balaji-Ganesh <balganeshboda27@gmail.com>
2022-11-18 11:13:28 +0530

    :lock: Missed commit.

--- config/firebase/firebase-config.js
+++ config/firebase/firebase-config.js
@@ -1,6 +1,6 @@
 const { credential } = require("firebase-admin");
 const firebaseAdmin = require("firebase-admin");
-const serviceAccountKey = require("./serviceAccountKey_experiment.json");   // currently this is of experiemnt project. Once tests done, switch to real one.
+const serviceAccountKey = require("./serviceAccountKey_experiment.json"); // currently this is of experiemnt project. Once tests done, switch to real one.
 
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccountKey),
