4f7361d8355152f631b19bfaf501ba495c2730a3
Balaji-Ganesh <balganeshboda27@gmail.com>
2022-11-18 13:22:06 +0530

    :art: #3 Campaign creation -- with firestore

--- controllers/campaigns.controller.js
+++ controllers/campaigns.controller.js
@@ -1,3 +1,12 @@
+const firebaseAdmin = require("../config/firebase/firebase-config");
+
+// for firestore..
+const firestoreDB = firebaseAdmin.firestore();
+
+// Collections working on..
+const campaigns = firestoreDB.collection("campaigns");
+const users = firestoreDB.collection("users");
+
 exports.recentCampaigns = (req, res) => {
   /* What to do here??..
       0. Extract the body.
@@ -48,9 +57,9 @@ exports.viewCampaign = (req, res) => {
   }
 };
 
-exports.createCampaign = (req, res) => {
+exports.createCampaign = async (req, res) => {
   /* What to do here??..
-      0. Extract the body.
+      0. Extract the `req.body` & get the UID from `req.user.UID`.
       1. Store the data in blockchain, deploy it. 
       (after successful deploy)
       2. Create a miniature document(campaign title, desc, goal, UID, ) with fundraiser ID -- get the UID from `req.user`.
@@ -61,20 +70,71 @@ exports.createCampaign = (req, res) => {
     */
   try {
     //STEP-0: extract the data received...
-    const [campaignTitle, campaignDescription] = req.body; // still more fields..
+    // console.info(req.body);
+    const {
+      bannerUrl,
+      title,
+      description,
+      ethRaised,
+      category,
+      deadline,
+      walletAddress,
+    } = req.body;
 
     // STEP-1: ..
 
-    // STEP-2: ..
+    // STEP-2: Create a mini version in firestore.
+    const UID = "2YuEvu0vwXcvppRn9fvqRO1LJjF3"; // currently hard-coding, but should get dynamically.
+    const uniqueId = Date.now();
+    await campaigns
+      .doc("" + uniqueId) // actually should be the unique address of contract after deployment.
+      .set({
+        title: title,
+        description: description,
+        bannerUrl: bannerUrl,
+        campaignStatus: "ACTIVE",
+        ethRaised: ethRaised,
+        category: category,
+        ethFunded: 0,
+        deadline: deadline,
+        raisedBy: UID, // should be req.user.UID
+        walletAddress: walletAddress,
+      });
 
-    // STEP-3: ..
+    // STEP-3: Attach it to the user..
+    // Get the stored document -- for testing purpose..
+    const campaign = await campaigns.doc("" + uniqueId).get();
+
+    const user = users.doc("" + UID); // get the UID, to whom to be appended.
+    if ((await user.get()).exists) {
+      await user.update({
+        campaignsRaised: firebaseAdmin.firestore.FieldValue.arrayUnion(
+          campaign.id
+        ),
+      });
+    } else {
+      user.set({
+        campaignsRaised: [campaign.id],
+      });
+    }
 
     // STEP-4:
     // make decision..
-    return res.status(200).json("Create campaign route");
+
+    // return res.status(200).json("Create campaign route");
+    // Query for the document created...
+    if (campaign.exists)
+      return res.status(200).json({
+        campaignId: campaign.id,
+      });
+    // based on this id, user-vew will navigate to the campaign page.
+    else return res.status(200).json({ msg: "No such campaign" });
   } catch (err) {
     // NOTE: decide status code based on the error..
-    return res.status(500).json({ msg: "error-message" });
+    console.error(err);
+    return res
+      .status(500)
+      .json({ msg: "Sorry, an error occured while working." });
   }
 };
 
@@ -85,7 +145,8 @@ exports.fundCampaign = (req, res) => {
     1. Perform transaction in smart contract.
     (after successful transaction)
     2. Update the amount raised in firestore doc.
-    3. based on the result, send appropriate message.
+    3. Update the funder profile with the campaign (address) he/she contributed for.
+    4. based on the result, send appropriate message.
   */
   try {
     // STEP-0:
